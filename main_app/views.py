c31c146b77d2cd0fc64ac3240d75eff18d4c4a23
bathant702 <bathant702nv@gmail.com>
2023-05-16 00:52:20 -0700

    Game model established

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from .models import Game
 
 #dummy data
-games = [
-    {'name': 'Magic: The Gathering', 'genre': ' collectible card game', 'players': '2 to many', 'playtime': '20-60 min', 'description': 'family friendly fantasy card game '},
-    {'name': 'Monopoly', 'genre': 'board game', 'players': '2 to 4', 'playtime': '180 minutes', 'description': 'family friendly board game about economics'},
-    {'name': 'Catan', 'genre': 'board game', 'players': '2 to 6', 'playtime': '120 minutes', 'description': 'family friendly board game about bricks and sheep'},
-]
+# games = [
+#     {'name': 'Magic: The Gathering', 'genre': ' collectible card game', 'players': '2 to many', 'playtime': '20-60 min', 'description': 'family friendly fantasy card game '},
+#     {'name': 'Monopoly', 'genre': 'board game', 'players': '2 to 4', 'playtime': '180 minutes', 'description': 'family friendly board game about economics'},
+#     {'name': 'Catan', 'genre': 'board game', 'players': '2 to 6', 'playtime': '120 minutes', 'description': 'family friendly board game about bricks and sheep'},
+# ]
+
 
 # Create your views here.
 
@@ -19,6 +21,7 @@ def about(request):
     return render(request, 'about.html')
 
 def games_index(request):
+    games = Game.objects.all() #retrieving Game model
     return render(request, 'games/index.html', {
         'games': games
     })
