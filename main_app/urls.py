0408981fbcce57600a04fd22bbd7f9b55ead47b8
bathant702 <bathant702nv@gmail.com>
2023-05-16 16:30:13 -0700

    details completed

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'), #view path to home
     path('about/', views.about, name='about'), #views path to about
     path('games/', views.games_index, name='index'), #index path to list
+    path('games/<int:game_id>/', views.games_detail, name ='detail'), #detail path to list
 ]
